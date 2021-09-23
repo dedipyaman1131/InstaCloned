e2dd139d6c27913b80ca891c5d1bd31a2d3013b7
dedipyaman1131 <74338520+dedipyaman1131@users.noreply.github.com>
2021-09-23 13:31:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const bcrypt = require('bcrypt')
+const saltRoununds = 10;
+const PORT = 3000;
+
+//ejs
+app.set('view engine', 'ejs');
+
+//css
+app.use(express.static('public/src'));
+//photos
+app.use(express.static(__dirname + ('public/photos')))
+
+
+app.get('/',(req,res)=>{
+    res.render('interface')
+});
+app.get('/login',(req,res)=>{
+    res.render('login')
+});
+
+app.get('/signup',(req,res)=>{  
+    res.render('signup')
+}
+)
+
+
+app.listen(PORT,(req,res)=>{
+    console.log(`server is running on ${PORT}`);
+})
