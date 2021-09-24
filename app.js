5d816a26a6b43286c5dc134c802cb0c962e59386
dedipyaman1131 <74338520+dedipyaman1131@users.noreply.github.com>
2021-09-24 08:54:57 +0530

    login signup page updated

--- app.js
+++ app.js
@@ -27,8 +27,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/signup',(req,res)=>{  
     res.render('signup')
-}
-)
+});
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
 
 
 app.listen(PORT,(req,res)=>{
